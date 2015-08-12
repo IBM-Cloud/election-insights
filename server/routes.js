9e4ae2a0db1961fc15955ed094edd862810a4888
Jonathan Kaufman <jdkaufma@us.ibm.com>
2015-08-12 18:56:19 -0400

    prepare endpoint for querying and rolling up info from alchemy

--- server/routes.js
+++ server/routes.js
@@ -17,10 +17,104 @@
 var express = require('express');
 var router = express.Router();
 var alchemy = require('./alchemy');
+var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index');
 });
+
+/**
+ * Load news articles and flatten them in to name/value pairs for either entities, concepts, or keywords.
+ * Query params are:
+ *   start
+ *   end
+ *   type - one of 'entities', 'concepts', or 'keywords'. defaults to 'concepts' if not sepcified
+ */
+router.get('/newsinsights', function (req, res) {
+  var start = req.query.start || 'now-1d';
+  var end = req.query.end || 'now';
+  var type = req.query.type || 'concepts';
+
+  alchemy.news({
+    start: start,
+    end: end,
+    maxResults: 100,
+    return: getReturnInfoArray(type).join(',')
+  }, function (response) {
+    if (response.status === 'ERROR') {
+      res.status(400);
+      res.json(response);
+    } else {
+      res.json(collapseResponse(response));
+    }
+  });
+});
+
+/**
+ * Convert a response of that looks like {result: {docs: [ {source: {enriched: {url: {type: {text: ...
+ * to an array of names and values. The values represent how many times that entity/concept/keyword
+ * is represented in the response.
+ */
+function collapseResponse (response) {
+  // first build a map of {name: value} pairs
+  var map = {};
+  var arr = _(response.result.docs)
+    .map(function (d) {return d.source.enriched.url[type]})
+    .flatten()
+    .forEach(function (d) {
+      var newAmount = type === 'entities' ? d.count : 1;
+      if (map[d.text]) {
+        map[d.text] += newAmount;
+      } else {
+        map[d.text] = newAmount;
+      }
+    })
+    .value();
+  // convert the map into an array of {name: name, value: value} objects
+  var newResponse = [];
+  var awesome = _.forOwn(map, function (value, key) {
+    newResponse.push({name: key, value: value});
+  });
+  return newResponse.filter(function(d) {return d.value > 2});
+}
+
+/**
+ * Return an array of strings to request from AlchemyAPI
+ */
+function getReturnInfoArray (type) {
+  var baseReturnInfo = [
+    // 'enriched.url.title',
+    // 'enriched.url.docSentiment.type',
+    // 'enriched.url.docSentiment.score',
+    // 'enriched.url.docSentiment.mixed'
+  ];
+  var returnInfo;
+  switch (type) {
+    case 'entities':
+      returnInfo = baseReturnInfo.concat(
+        'enriched.url.entities.entity.text',
+        'enriched.url.entities.entity.type',
+        'enriched.url.entities.entity.sentiment',
+        'enriched.url.entities.entity.count'
+      );
+      break;
+
+    case 'concepts':
+      returnInfo = baseReturnInfo.concat('enriched.url.concepts.concept.text');
+      break;
+
+    case 'keywords':
+      returnInfo = baseReturnInfo.concat(
+        'enriched.url.keywords.keyword.text',
+        'enriched.url.keywords.keyword.sentiment'
+      );
+      break;
+
+    default:
+      // no op
+  }
+  return returnInfo;
+}
 
 module.exports = router;
