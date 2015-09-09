1e8ccb93b9f34ab21020b8f14880167768b9c831
Jonathan Kaufman <jdkaufma@us.ibm.com>
2015-09-09 13:22:49 -0400

    use react-bubble-chart from npm

--- client/components/BubbleChart.js
+++ client/components/BubbleChart.js
@@ -14,34 +14,36 @@
 // limitations under the License.
 //------------------------------------------------------------------------------
 
-import bubbleChartD3 from './bubbleChartD3';
-import React         from 'react';
+import React            from 'react';
+import ReactBubbleChart from 'react-bubble-chart';
+import Actions          from '../Actions';
+
+var colorLegend = [
+  //reds from dark to light
+  {color: "#67000d", text: 'Negative'}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
+  //neutral grey
+  {color: "#f0f0f0", text: 'Neutral'},
+  // blues from light to dark
+  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive'}
+];
 
 class BubbleChart extends React.Component {
   render () {
-    return <div className='bubble-chart-container'></div>;
-  }
+    var data = this.props.data.map(d => ({
+      _id: d._id,
+      value: d.value,
+      colorValue: d.sentiment,
+      selected: d.selected
+    }));
 
-  componentDidMount () {
-    bubbleChartD3.create(this.getDOMNode(), this.getChartState());
-  }
-
-  componentDidUpdate () {
-    bubbleChartD3.update(this.getDOMNode(), this.getChartState());
-  }
-
-  getChartState () {
-    return {
-      data: this.props.data
-    }
-  }
-
-  componentWillUnmount () {
-    bubbleChartD3.destroy(this.getDOMNode());
-  }
-
-  getDOMNode () {
-    return React.findDOMNode(this);
+    return <ReactBubbleChart
+      colorLegend={colorLegend}
+      data={data}
+      selectedColor="#737373"
+      selectedTextColor="#d9d9d9"
+      fixedDomain={{min: -1, max: 1}}
+      onClick={Actions.loadArticlesForEntity.bind(Actions)}
+    />;
   }
 }
 
