948f4965e0e5d898a3bf5fa5191cbb99f2fd3c52
Jonathan Kaufman <jdkaufma@us.ibm.com>
2015-09-17 13:50:14 -0400

    update bubble chart version - allows for better styling of different sized bubbles and more fine tuned legend adjustments

--- client/components/BubbleChart.js
+++ client/components/BubbleChart.js
@@ -19,12 +19,26 @@ import ReactBubbleChart from 'react-bubble-chart';
 import Actions          from '../Actions';
 
 var colorLegend = [
-  //reds from dark to light
-  {color: "#67000d", text: 'Negative'}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
+  // reds from dark to light
+  {color: "#67000d", textColor: '#fee0d2', text: 'Negative'},
+  {color: "#a50f15", textColor: '#fee0d2'},
+  {color: "#cb181d", textColor: '#fee0d2'},
+  "#ef3b2c",
+  "#fb6a4a",
+  "#fc9272",
+  "#fcbba1",
+  "#fee0d2",
   //neutral grey
   {color: "#f0f0f0", text: 'Neutral'},
   // blues from light to dark
-  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive'}
+  "#deebf7",
+  "#c6dbef",
+  "#9ecae1",
+  "#6baed6",
+  "#4292c6",
+  {color: "#2171b5", textColor: '#deebf7'},
+  {color: '#08519c', textColor: '#deebf7'},
+  {color: "#08306b", textColor: '#deebf7', text: 'Positive'}
 ];
 
 class BubbleChart extends React.Component {
@@ -38,6 +52,7 @@ class BubbleChart extends React.Component {
 
     return <ReactBubbleChart
       colorLegend={colorLegend}
+      legend={true}
       data={data}
       selectedColor="#737373"
       selectedTextColor="#d9d9d9"
